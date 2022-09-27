2427a95d7766050bff8368c07528dbe3fdd10400
JJK70 <jjk701@gmail.com>
2022-09-26 20:18:54 -0400

    add show, edit files, controllers and routes

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,5 +1,19 @@
 import { Flight } from "../models/flight.js"
 
+function index(req, res) {
+  Flight.find({})
+  .then(flights => {
+    res.render('flights/index', {
+      flights: flights,
+      title: 'All Flights',
+      time: req.time
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
 
 function newFlight(req, res) {
   res.render('flights/new', {
@@ -8,7 +22,78 @@ function newFlight(req, res) {
   })
 }
 
+function create(req, res) {
+  req.body.nowShowing = !!req.body.nowShowing
+  if (req.body.cast) {
+    req.body.cast = req.body.cast.split(', ')
+  }
+  Flight.create(req.body)
+  .then(movie => {
+    res.redirect(`/flights/new`)
+  })
+  .catch(err => {
+    res.redirect('/flights/new')
+  })
+}
+
+
+
+function show(req, res) {
+  Flight.findById(req.params.id)
+  .then(flight => {
+    res.render('flights/show', {
+      flight: flight
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+function deleteSkill(req, res) {
+  Flight.findByIdAndDelete(req.params.id)
+  .then(flight => {
+    res.redirect('/flights')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/flights')
+  })
+}
+
+function edit(req, res) {
+  Flight.findById(req.params.id)
+  .then(flight => {
+    res.render('flights/edit', {
+      flight: flight
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/flight')
+  })
+}
+
+function update(req, res) {
+  req.body.flight = !!req.body.flight
+  Flight.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then(flight => {
+    res.redirect(`/flight/${flight._id}`)
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/flights')
+  })
+}
+
 
 export {
-  newFlight as new 
+  index,
+  newFlight as new,
+  create,
+  show,
+  deleteSkill as delete,
+  edit,
+  update 
 }
--- routes/flights.js
+++ routes/flights.js
@@ -5,11 +5,15 @@ import * as flightsCtrl from '../controllers/flights.js'
 
 // Get new flights
 router.get('/new', flightsCtrl.new)
+router.post('/', flightsCtrl.create)
+router.get('/', flightsCtrl.index)
+router.get('/new', flightsCtrl.new)
+router.get('/:id', flightsCtrl.show)
+router.get('/:id/edit', flightsCtrl.edit)
+router.post('/', flightsCtrl.create)
+router.delete('/:id', flightsCtrl.delete)
+router.put('/:id', flightsCtrl.update)
 
-/* get / flights */
-// router.get('/', flightsCtrl.index)
-
-// router.post('/', flightsCtrl.create)
 
 export {
   router
