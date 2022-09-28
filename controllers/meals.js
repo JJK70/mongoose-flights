b89b5315faedf4ad5503d08220337116e720f6ef
JJK70 <jjk701@gmail.com>
2022-09-28 13:03:58 -0400

    add meal create function and route

--- controllers/meals.js
+++ controllers/meals.js
@@ -10,6 +10,13 @@ function newMeal(req, res) {
   })
 }
 
+function create(req, res) {
+  Meal.create(req.body)
+  .then(meal => 
+    res.redirect('/meals,new'))
+}
+
 export {
   newMeal as new,
+  create,
 }
